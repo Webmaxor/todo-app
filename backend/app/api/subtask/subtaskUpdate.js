1c1f2e7bf494d41d58316c8e7829909d0cf64a73
Webmaxor <webmaxor@gmail.com>
2021-08-15 15:18:10 +0500

    Update parent task's status based on subtask state

--- backend/app/api/subtask/subtaskUpdate.js
+++ backend/app/api/subtask/subtaskUpdate.js
@@ -1,6 +1,6 @@
 const validator = require("express-joi-validation").createValidator({});
 const Joi = require("joi");
-const { Subtask } = require("../../models");
+const { Todo, Subtask } = require("../../models");
 
 const paramsSchema = Joi.object({
   id: Joi.number().integer().positive().required(),
@@ -36,6 +36,36 @@ module.exports.subtaskUpdate = async ({ body, params }, res) => {
       status,
       updatedAt: new Date(),
     });
+
+    const task = await Todo.findOne({
+      where: { id: subtask.todoId },
+      include: Subtask,
+    });
+
+    if (task) {
+      const completedSubTasksCount = task.Subtasks.reduce(
+        (acc, item) => (item.status === "completed" ? 1 : 0) + acc,
+        0
+      );
+
+      if (
+        task.Subtasks.length === completedSubTasksCount &&
+        task.status === "pending"
+      ) {
+        await task.update({
+          status: "completed",
+          updatedAt: new Date(),
+        });
+      } else if (
+        task.Subtasks.length !== completedSubTasksCount &&
+        task.status === "completed"
+      ) {
+        await task.update({
+          status: "pending",
+          updatedAt: new Date(),
+        });
+      }
+    }
   } catch (error) {
     console.log(error);
     return;
